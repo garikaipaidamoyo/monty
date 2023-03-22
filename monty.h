2ae3cebf268765b9d6ef53415b2eaa0287242730
grayolaRSA <gtndanga@gmail.com>
2023-03-22 23:25:29 +0200

    added opcode instructions

--- monty.h
+++ monty.h
@@ -1,7 +1,42 @@
 #ifndef MONTY_H
 #define MONTY_H
 
+/**
+ * struct stack_s - doubly linked list representation of a stack (or queue)
+ * @n: integer
+ * @prev: points to the previous element of the stack (or queue)
+ * @next: points to the next element of the stack (or queue)
+ *
+ * Description: doubly linked list node structure
+ * for stack, queues, LIFO, FIFO
+ */
+typedef struct stack_s
+{
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
+} stack_t;
+
+
+/**
+ * struct instruction_s - opcode and its function
+ * @opcode: the opcode
+ * @f: function to handle the opcode
+ *
+ * Description: opcode and its function
+ * for stack, queues, LIFO, FIFO
+ */
+typedef struct instruction_s
+{
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
+} instruction_t;
+
 void push(Stack *stack, int value, int line_num);
 void pall(Stack *stack);
-
+void nop(stack_t **dlist, unsigned int c_line);
+void add(stack_t **dlist, unsigned int c_line);
+void swap(stack_t **dlist, unsigned int c_line);
+void pop(stack_t **dlist, unsigned int c_line);
+void pint(stack_t **dlist, unsigned int c_line);
 #endif /*MONTY_H*/
